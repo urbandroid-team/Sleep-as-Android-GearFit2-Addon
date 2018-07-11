87b9e7d80206af9e9a2c656557dd1cbc8fbac79c
artaud <richtej2@gmail.com>
2018-07-11 10:36:58 +0200

    project created

---
+++ inc/sleepgearfit.h
@@ -0,0 +1,18 @@
+#ifndef __sleepgearfit_H__
+#define __sleepgearfit_H__
+
+#include <watch_app.h>
+#include <watch_app_efl.h>
+#include <Elementary.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "sleepgearfit"
+
+#if !defined(PACKAGE)
+#define PACKAGE "com.urbandroid.sleepgearfit"
+#endif
+
+#endif /* __sleepgearfit_H__ */
