e2d0bba52cab1993e79b865211bfea48582d84e8
Unknown <george.barnett.121@gmail.com>
2018-07-12 01:26:13 -0700

    Added Watchface and Service Projects
    started service from watchface

---
+++ Service/inc/digitialwatch2.h
@@ -0,0 +1,12 @@
+#ifndef __digitialwatch2_H__
+#define __digitialwatch2_H__
+
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "digitialwatch2"
+
+
+#endif /* __digitialwatch2_H__ */
---
+++ WatchFace/Debug/.d
@@ -0,0 +1,6 @@
+res/edje/main.edj: ../res/edje/../../inc/view_defines.h \
+  ./../res/images/cipher_board_bg.png \
+  ./../res/images/cipher_board_ring.png \
+  ./../res/images/cipher_board_shadow.png \
+  ./../res/images/colon.png \
+  ./../res/images/second_hand.png
